bb7799f106a08652313c346f0493c41b5b87b137
drifa <drifasoley@gmail.com>
2022-01-23 15:43:19 +0000

    Updates

---
+++ precache-manifest.903262e4cb45e67199dfed74e75b489f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c30004e2f3e96ac4c3cd1d98866ce23",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "d49e51564e59f503a8c9",
+    "url": "/robofriends/static/css/main.7bf2273d.chunk.css"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/robofriends/static/js/2.d679fc0b.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/robofriends/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "d49e51564e59f503a8c9",
+    "url": "/robofriends/static/js/main.2c7ef64a.chunk.js"
+  },
+  {
+    "revision": "04ba49df8c75a51ba00d",
+    "url": "/robofriends/static/js/runtime~main.8bd9438d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
